8e115ac7b1db9abd0c574562e4da69801aa618f5
Seris <mathieu.seris@student.ecp.fr>
2017-12-04 14:11:59 +0100

    v0

---
+++ infirmiers/soins_app/models/form.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class Soins(forms.Form):
+    nom_soin =
+    type_soin
+    adresse_soin
+    frequence_soin =
+    ponctualite_definie =
--- infirmiers/soins_app/models/soins.py
+++ infirmiers/soins_app/models/soins.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Soin(models.Model):
     nom_soin=models.CharField(max_length=100)
-    type_soin=
-    adresse_soin=
-    frequence_soin=
+    type_soin= models.CharField(max_length=100)
+    adresse_soin= models.CharField(max_length=100)
+    frequence_soin= models.CharField(max_length=100)
     ponctualite_definie=
