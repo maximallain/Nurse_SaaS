a140ef54686b3113bc1ab853fffd98cceac6bba5
Alexandre Sioufi <sioufia78@gmail.com>
2017-12-05 18:11:25 +0100

    Added the detail view of a nurse

--- infirmiers/infirmiers_app/urls.py
+++ infirmiers/infirmiers_app/urls.py
@@ -1,8 +1,22 @@
 from django.conf.urls import url, include
 
 from .views.accueil import accueil
+<<<<<<< HEAD
 
 urlpatterns = [
     url('^', include('django.contrib.auth.urls')),
     url('^accueil',accueil, name='accueil' )
+=======
+from .views.createNurse import nurse_creation_view
+from .views.nurse_list import NurseListView
+from .views.nurse_detail import NurseDetailView
+
+urlpatterns = [
+    url('^', include('django.contrib.auth.urls')),
+    url('^accueil',accueil, name='accueil' ),
+    url('^creation',nurse_creation_view, name="nurse_creation_view"),
+    url('^nurses$', NurseListView.as_view(), name='nurse_list'),
+    url('^nurse/(?P<pk>\d+)$', NurseDetailView.as_view(), name='nurse_detail')
+
+>>>>>>> Added the detail view of a nurse
 ]
