8b2d9e1c293a25cc973146f5805b570de776aaf8
Seris <mathieu.seris@student.ecp.fr>
2017-12-08 14:36:00 +0100

    ajout patient v0.1

--- infirmiers/soins_app/forms/form.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class Soins(forms.Form):
-    nom_soin = forms.CharField(max_length=100)
-    type_soin = forms.CharField(max_length=100)
-    adresse_soin = forms.CharField(max_length=100)
-    frequence_soin = forms.CharField(max_length=100)
-    ponctualite_definie = forms.CharField(max_length=100)
