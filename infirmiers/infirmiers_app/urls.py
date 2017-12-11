aa126d6f30909e26abc4f3e8eb0de42951a45968
Alexandre Sioufi <sioufia78@gmail.com>
2017-12-11 14:07:45 +0100

    Corrected the previous commit

--- infirmiers/infirmiers_app/urls.py
+++ infirmiers/infirmiers_app/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url, include
 
-from .views.accueil import accueil
 from .views.createNurse import nurse_creation_view
 from .views.nurse_list import NurseListView
 from .views.nurse_detail import NurseDetailView
@@ -8,7 +7,6 @@ from .views.create_availability import availability_creation_view
 
 urlpatterns = [
     url('^', include('django.contrib.auth.urls')),
-    url('^accueil',accueil, name='accueil' ),
     url('^creation',nurse_creation_view, name="nurse_creation_view"),
     url('^nurses$', NurseListView.as_view(), name='nurse_list'),
     url('^nurses/(?P<nurse_id>\d+)', NurseListView.as_view(), name='nurse_list'),
