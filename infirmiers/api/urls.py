4d182145b9a8830ece4e253c50049bb9f65b5398
Alexandre Sioufi <sioufia78@gmail.com>
2017-12-15 14:54:47 +0100

    Fixed Url conflicts with API

--- infirmiers/api/urls.py
+++ infirmiers/api/urls.py
@@ -6,8 +6,8 @@ from .views.visit_detail import visit_detail
 
 urlpatterns = [
     url('^', include('django.contrib.auth.urls')), 
-    url('^nurses', nurses_list, name="nurses_list"),
-    url('^nurse/(?P<pk>[0-9]+)/$', nurse_detail, name="nurse_detail"),
-    url('^visits', visits_list, name="visits_list"),
-    url('^visit/(?P<pk>[0-9]+)/$', visit_detail, name="visit_detail")
+    url('^nurses', nurses_list, name="nurses_list_api"),
+    url('^nurse/(?P<pk>[0-9]+)/$', nurse_detail, name="nurse_detail_api"),
+    url('^visits', visits_list, name="visits_list_api"),
+    url('^visit/(?P<pk>[0-9]+)/$', visit_detail, name="visit_detail_api")
 ]
