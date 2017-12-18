c55e2dce7f8fec7681cc427f59eb770a286d8bee
Alexandre Sioufi <sioufia78@gmail.com>
2017-12-19 00:10:23 +0100

    Added the Post method to update the fields of visits

--- infirmiers/api/urls.py
+++ infirmiers/api/urls.py
@@ -3,12 +3,15 @@ from .views.nurses_list import NursesList
 from .views.visits_list import VisitList
 from .views.nurse_detail import nurse_detail
 from .views.visit_detail import visit_detail
+from .views.visits_list_post import VisitsListPostAPIView
 
 urlpatterns = [
     url('^', include('django.contrib.auth.urls')), 
     url('^nurses/(?P<officepk>[0-9]+)/$', NursesList.as_view(), name="nurses_office_list_api"),
     url('^nurses/$', NursesList.as_view(), name="nurses_office_list_api"),
     url('^nurse/(?P<pk>[0-9]+)/$', nurse_detail, name="nurse_detail_api"),
+    url('^visits/create', VisitsListPostAPIView.as_view(), name="visits_list_api_create"),
     url('^visits/', VisitList.as_view(), name="visits_list_api"),
-    url('^visit/(?P<pk>[0-9]+)/$', visit_detail, name="visit_detail_api")
+    url('^visit/(?P<pk>[0-9]+)/$', visit_detail, name="visit_detail_api"),
+    
 ]
--- infirmiers/soins_app/models/visits.py
+++ infirmiers/soins_app/models/visits.py
@@ -4,6 +4,7 @@ from infirmiers_app.models.nurse import Nurse
 
 class Visit(models.Model):
     date = models.DateField(default=date.today)
+    time = models.TimeField(null = True)
     completed = models.BooleanField(default="False")
     #default duration visit is 2hours - MVP
     duration_visit = models.IntegerField(default=2)
