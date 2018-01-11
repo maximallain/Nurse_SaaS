0e3b051ac4dfb5982b5b9c6c904ca896a87c136b
maximallain <maxime.allain@student.ecp.fr>
2018-01-11 17:42:39 +0100

    Ajout du planning

--- infirmiers/soins_app/urls.py
+++ infirmiers/soins_app/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 
+from .views.soin_detail import SoinDetailView
 from .views.soin_request import soin_request
 from .views.patient_request import patient_request
 from .views.patient_list import PatientListView
@@ -12,4 +13,6 @@ urlpatterns = [
     url('^creation_soins/(?P<patient_id>\d+)', soin_request, name='soin_request'),
     url('^creation_patient', patient_request, name='patient_request'),
     url('^patient/(?P<pk>\d+)$', PatientDetailView.as_view(), name='patient_detail'),
+    url('^soin/(?P<patient_pk>\d+)/(?P<treatment_pk>\d+)$', SoinDetailView.Soin_Detail, name='soin_detail'),
+
 ]
