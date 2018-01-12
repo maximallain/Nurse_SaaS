d9b4503f3bc29a41dae30f75c1bb86ae29e3b1c3
Quentin Rossettini <quentin.rossettini@mail.utoronto.ca>
2018-01-12 11:51:54 +0100

    added an address check when creating office or patient

--- infirmiers/patients_app/views/patient_request.py
+++ infirmiers/patients_app/views/patient_request.py
@@ -5,6 +5,10 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from signUp.models.office import Office
 from django.contrib.auth.decorators import login_required
+import requests as req
+
+KEY = "AIzaSyATtwrFvepaVpvY0oYYyY_G71Mk97D7yzo"
+
 
 @login_required
 def patient_request(request):
@@ -13,22 +17,28 @@ def patient_request(request):
         form = Patients_Form(request.POST)
 
         if form.is_valid():
-            FirstName = form.cleaned_data['FirstName'].capitalize()
-            LastName = form.cleaned_data['LastName'].upper()
             Adress = form.cleaned_data['Adress']
-            PhoneNumber = form.cleaned_data['PhoneNumber']
-            Email = form.cleaned_data['Email']
-            office = Office.objects.filter(user = request.user )[0]
+            if not req.get("https://maps.googleapis.com/maps/api/distancematrix/json?origins=" +
+                           Adress.replace(" ", "%20") + "&destinations=" + Adress.replace(" ", "%20")
+                           + "&key=" + KEY).json().get("destination_addresses")[0] == "":
+                FirstName = form.cleaned_data['FirstName'].capitalize()
+                LastName = form.cleaned_data['LastName'].upper()
+                PhoneNumber = form.cleaned_data['PhoneNumber']
+                Email = form.cleaned_data['Email']
+                office = Office.objects.filter(user = request.user )[0]
 
-            #Check telephone can be converted to an integer
-            try:
-                int(PhoneNumber)
-            except ValueError:
-                error_message = "The number must be numerical"
-                return render(request, 'nouveau_patient.html', {'form': form, 'error_message' : error_message})
-    
-            Patient(LastName = LastName, FirstName = FirstName, Adress = Adress, PhoneNumber = PhoneNumber,Email = Email,office = office).save()
-            return HttpResponseRedirect(reverse("patient_list"))
+                #Check telephone can be converted to an integer
+                try:
+                    int(PhoneNumber)
+                except ValueError:
+                    error_message = "The number must be numerical"
+                    return render(request, 'nouveau_patient.html', {'form': form, 'error_message' : error_message})
+
+                Patient(LastName = LastName, FirstName = FirstName, Adress = Adress, PhoneNumber = PhoneNumber,Email = Email,office = office).save()
+                return HttpResponseRedirect(reverse("patient_list"))
+            else:
+                error_message = "Invalid address"
+                return render(request, 'nouveau_patient.html', {'form': form, 'error_message': error_message})
     else:
         form = Patients_Form()
 
--- infirmiers/signUp/views.py
+++ infirmiers/signUp/views.py
@@ -5,26 +5,35 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from .forms import SignUpForm, SignUpFormOffice
 from .models.office import Office
+import requests as req
+from patients_app.views.patient_request import KEY
 
 def signup(request):
     if request.method == 'POST':
         user_form = SignUpForm(request.POST)
         office_form = SignUpFormOffice(request.POST)
         if user_form.is_valid() and office_form.is_valid():
-            user_form.save()
-
-            username = user_form.cleaned_data.get('username')
-            user_current = User.objects.filter(username = username)[0]
-
             office_adress = office_form.cleaned_data['adress']
-            office = Office(adress=office_adress, user = user_current)
-            office.save()
+            if not req.get("https://maps.googleapis.com/maps/api/distancematrix/json?origins=" +
+                           office_adress.replace(" ", "%20") + "&destinations=" + office_adress.replace(" ", "%20")
+                           + "&key=" + KEY).json().get("destination_addresses")[0] == "":
 
-            raw_password = user_form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            office_pk = Office.objects.filter(user = user)[0].pk
-            return HttpResponseRedirect(reverse("home"))
+                user_form.save()
+
+                username = user_form.cleaned_data.get('username')
+                user_current = User.objects.filter(username = username)[0]
+
+                office = Office(adress=office_adress, user = user_current)
+                office.save()
+
+                raw_password = user_form.cleaned_data.get('password1')
+                user = authenticate(username=username, password=raw_password)
+                login(request, user)
+                office_pk = Office.objects.filter(user = user)[0].pk
+                return HttpResponseRedirect(reverse("home"))
+            else:
+                error_message = "Invalid address"
+                return render(request, 'registration/signup.html', {'user_form': user_form, 'office_form' : office_form, 'error_message': error_message})
     else:
         user_form = SignUpForm()
         office_form = SignUpFormOffice()
