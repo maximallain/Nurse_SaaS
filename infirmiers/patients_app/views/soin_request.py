85e8ce9df418d3570d9be8af924be837813da391
Quentin Rossettini <quentin.rossettini@mail.utoronto.ca>
2018-01-12 12:13:48 +0100

    added error when date before today when creating treatment

--- infirmiers/patients_app/views/soin_request.py
+++ infirmiers/patients_app/views/soin_request.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from patients_app.models.Patients import Patient
 from django.contrib.auth.decorators import login_required
+import datetime
 
 @login_required
 def soin_request(request, patient_id):
@@ -13,28 +14,35 @@ def soin_request(request, patient_id):
         form = Soins(request.POST)
 
         if form.is_valid():
-            name_soin = form.cleaned_data['name_soin']
-            type_soin = form.cleaned_data['type_soin']
-            frequence_soin = form.cleaned_data['frequence_soin']
-            specific_visit_time = form.cleaned_data['specific_visit_time']
             start_date=form.cleaned_data['start_date']
-            treatment_duration=form.cleaned_data['treatment_duration']
+            try:
+                if start_date > datetime.date.today():
+                    name_soin = form.cleaned_data['name_soin']
+                    type_soin = form.cleaned_data['type_soin']
+                    frequence_soin = form.cleaned_data['frequence_soin']
+                    specific_visit_time = form.cleaned_data['specific_visit_time']
+                    treatment_duration=form.cleaned_data['treatment_duration']
 
-            soin =  Soin(name_soin=name_soin,
-                 type_soin = type_soin,
-                 frequence_soin = frequence_soin,
-                 specific_visit_time = specific_visit_time,
-                 start_date=start_date,
-                 treatment_duration=treatment_duration,
-                 patient = patient_id
-                 )
-            #envoi = True
-            soin.save()
+                    soin =  Soin(name_soin=name_soin,
+                         type_soin = type_soin,
+                         frequence_soin = frequence_soin,
+                         specific_visit_time = specific_visit_time,
+                         start_date=start_date,
+                         treatment_duration=treatment_duration,
+                         patient = patient_id
+                         )
+                    #envoi = True
+                    soin.save()
 
-            patient = Patient.objects.get(pk = patient_id)
-            patient.treatments.add(soin)
+                    patient = Patient.objects.get(pk = patient_id)
+                    patient.treatments.add(soin)
 
-            return HttpResponseRedirect(reverse("patient_detail", args=[patient_id]))
+                    return HttpResponseRedirect(reverse("patient_detail", args=[patient_id]))
+                else:
+                    return render(request, 'nouveau_soin.html', {'error_message' : "Invalid date", 'form': form, 'patient_pk': patient_id})
+            except ValueError:
+                return render(request, 'nouveau_soin.html',
+                              {'error_message': "Invalid date", 'form': form, 'patient_pk': patient_id})
 
     else:
         form = Soins()
